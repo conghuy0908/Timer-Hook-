57458180ba75e02bc75e26a5c8e15cb798d4c932
conghuy0908 <102871202+conghuy0908@users.noreply.github.com>
2022-11-08 18:26:56 +0700

    fixx code

--- Timer.js
+++ Timer.js
@@ -6,7 +6,10 @@ function Timer() {
             setTime((time) => time - 1);
             }, 1000);
             return () => clearTimeout(timer);
-        }, []);
+        }, [time]);
+        if(time === 0){
+            alert("Time's Up")
+        }
     return <h1>Time : {time}</h1>
 };
 export default Timer;
